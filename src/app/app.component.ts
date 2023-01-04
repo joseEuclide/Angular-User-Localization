67f8e9cae5d5056171ee6406f556e85e572684f2
joseEuclide <euclidessdossantoss@gmail.com>
2023-01-04 14:17:47 -0100

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UserLocalization';
+  lat = 0
+  longi = 0
+
+  ngOnInit(){
+     this.currentUserLocalization();
+  }
+  currentUserLocalization(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.longi = position.coords.longitude;
+      })
+    }else{
+      alert("Geolocalization is not supported in this browser, use other.")
+    }
+  }
 }
